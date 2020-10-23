179d2c8479f22e48782d4a04ee54b39eb453935b
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-10-23 17:53:01 +0900

    半角英数かどうかを判別するStringの拡張を追加

--- Login_SwiftUI/Login_SwiftUI/Common/Extensions/String+Extensions.swift
+++ Login_SwiftUI/Login_SwiftUI/Common/Extensions/String+Extensions.swift
@@ -5,4 +5,9 @@
 //  Created by home on 2020/10/23.
 //
 
-
+extension String {
+    // 半角英数かどうか判別
+    var isAlphanumeric: Bool {
+        return !isEmpty && range(of: "[^a-zA-Z0-9]", options: .regularExpression) == nil
+    }
+}
