6b8ecee8058fda5f1f3468fd4c2c60ccbb3ff98b
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-10-23 18:57:25 +0900

    idとpasswordが入力エラー対象の文字列になった場合変数isValidTextの状態を変更

--- Login_SwiftUI/Login_SwiftUI/Login/LoginViewModel.swift
+++ Login_SwiftUI/Login_SwiftUI/Login/LoginViewModel.swift
@@ -5,11 +5,34 @@
 //  Created by home on 2020/10/23.
 //
 
-import SwiftUI
+import Combine
 
 class LoginViewModel: ObservableObject {
     @Published var id: String = ""
     @Published var password: String = ""
     @Published var isValidText: Bool = false
     
+    private var disposables = [AnyCancellable]()
+    
+    init() {
+        $id
+            .sink(receiveValue: {
+                if !$0.isAlphanumeric && !$0.isEmpty {
+                    self.isValidText = false
+                } else {
+                    self.isValidText = true
+                }
+            })
+            .store(in: &disposables)
+        
+        $password
+            .sink(receiveValue: {
+                if !$0.isAlphanumeric && !$0.isEmpty {
+                    self.isValidText = false
+                } else {
+                    self.isValidText = true
+                }
+            })
+            .store(in: &disposables)
+    }
 }
