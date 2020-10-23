0edb557ea37360e8e65497515634cc2be70078ee
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-10-23 17:01:11 +0900

    アカウントIDログインのViewを追加

---
+++ Login_SwiftUI/Login_SwiftUI/Login/LoginView.swift
@@ -0,0 +1,36 @@
+//
+//  LoginView.swift
+//  Login_SwiftUI
+//
+//  Created by home on 2020/10/21.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @ObservedObject var viewModel: LoginViewModel
+    
+    init(viewModel: LoginViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 25, content: {
+            Text("アカウントIDでログイン")
+            
+            TextField("IDを入力", text: $viewModel.id)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 300)
+            
+            TextField("パスワードを入力", text: $viewModel.password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 300)
+        })
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView(viewModel: LoginViewModel())
+    }
+}
