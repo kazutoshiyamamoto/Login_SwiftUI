4a1aa1c7a3c4cbc53f437ac5e8be6e4cb05704e8
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-10-23 17:10:37 +0900

    LoginのViewModelを追加

---
+++ Login_SwiftUI/Login_SwiftUI/Login/LoginViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewModel.swift
+//  Login_SwiftUI
+//
+//  Created by home on 2020/10/23.
+//
+
+import SwiftUI
+
+class LoginViewModel: ObservableObject {
+    @Published var id: String = ""
+    @Published var password: String = ""
+    
+    @Published var isInputError: Bool = true
+}
