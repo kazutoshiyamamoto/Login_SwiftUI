939aeb2d847668519936e40d680a78f38c3889d8
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-10-28 16:43:33 +0900

    UIViewRepresentableに準拠したLINEログインボタンの構造体を追加

--- Login_SwiftUI/Login_SwiftUI/Login/LineLoginButton.swift
+++ Login_SwiftUI/Login_SwiftUI/Login/LineLoginButton.swift
@@ -6,10 +6,49 @@
 //
 
 import SwiftUI
+import LineSDK
 
-struct LineLoginButton: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct LineLoginButton: UIViewRepresentable {
+    class Coordinator: NSObject, LoginButtonDelegate {
+        var parent: LineLoginButton
+        
+        init(_ parent: LineLoginButton) {
+            self.parent = parent
+        }
+        
+        func loginButton(_ button: LoginButton, didSucceedLogin loginResult: LoginResult) {
+             print("Login Succeeded.")
+         }
+         
+         func loginButton(_ button: LoginButton, didFailLogin error: LineSDKError) {
+             print("Error: \(error)")
+         }
+         
+         func loginButtonDidStartLogin(_ button: LoginButton) {
+             print("Login Started.")
+         }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+        return view
+    }
+    
+    func updateUIView(_ view: UIView, context: Context) {
+        let loginButton = LoginButton()
+        loginButton.delegate = context.coordinator
+        
+        loginButton.permissions = [.profile]
+        loginButton.presentingViewController = nil
+        
+        view.addSubview(loginButton)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 }
 
