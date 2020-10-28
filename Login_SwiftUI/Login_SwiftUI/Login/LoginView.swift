ee2dbf7eb17624c081dbbf82262d714b3fa2cee9
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-10-28 16:44:21 +0900

    アカウントIDログインに関わるUIの修正

--- Login_SwiftUI/Login_SwiftUI/Login/LoginView.swift
+++ Login_SwiftUI/Login_SwiftUI/Login/LoginView.swift
@@ -15,38 +15,35 @@ struct LoginView: View {
     }
     
     var body: some View {
-        VStack {
-            VStack(alignment: .leading, spacing: 15, content: {
-                Text("アカウントIDでログイン")
-                    .bold()
-                
-                // 入力した文字をLoginViewModelの変数idに通知
-                TextField("IDを入力（半角英数）", text: $viewModel.id)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .frame(width: 300)
-                
-                // 入力した文字をLoginViewModelの変数passwordに通知
-                TextField("パスワードを入力（半角英数）", text: $viewModel.password)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .frame(width: 300)
-            })
-            .padding(20)
+        VStack(alignment: .leading, spacing: 15) {
+            Text("アカウントIDでログイン")
+                .bold()
             
-            VStack(alignment: .center) {
-                Button(action: {
-                    // ボタンが選択されたことをLoginViewModelの変数isLoginButtonTappedに通知
-                    viewModel.isLoginButtonTapped = true
-                }) {
-                    Text("ログイン")
-                        .frame(width: 200, height: 50)
-                        .foregroundColor(Color.white)
-                        // ボタンの有効/無効状態に合わせて背景の色を変更
-                        .background(viewModel.isValidId && viewModel.isValidPassword ? Color.blue : Color.gray)
-                        .cornerRadius(10, antialiased: true)
-                }
-                // IDとパスワードがどちらも半角英数で入力されていればボタンを有効にする
-                .disabled(!viewModel.isValidId || !viewModel.isValidPassword)
+            // 入力した文字をLoginViewModelの変数idに通知
+            TextField("ID（半角英数）", text: $viewModel.id)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 300)
+            
+            // 入力した文字をLoginViewModelの変数passwordに通知
+            TextField("パスワード（半角英数）", text: $viewModel.password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 300)
+            
+            Button(action: {
+                // ボタンが選択されたことをLoginViewModelの変数isLoginButtonTappedに通知
+                viewModel.isLoginButtonTapped = true
+            }) {
+                Text("ログイン")
             }
+            .frame(width: 200, height: 45)
+            .foregroundColor(Color.white)
+            // ボタンの有効/無効状態に合わせて背景の色を変更
+            .background(viewModel.isValidId && viewModel.isValidPassword ? Color.blue : Color.gray)
+            .cornerRadius(10, antialiased: true)
+            // IDとパスワードがどちらも半角英数で入力されていればボタンを有効にする
+            .disabled(!viewModel.isValidId || !viewModel.isValidPassword)
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: 30, trailing: 0))
+            .alignmentGuide(.leading) { _ in -50 }
         }
     }
 }
