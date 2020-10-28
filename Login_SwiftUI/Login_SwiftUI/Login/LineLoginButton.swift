0b055c1ff672c5e0b806e9764d28f7446a82d8cf
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-10-28 16:42:11 +0900

    LINEログインボタンを実装するファイルを追加

---
+++ Login_SwiftUI/Login_SwiftUI/Login/LineLoginButton.swift
@@ -0,0 +1,20 @@
+//
+//  LineLoginButton.swift
+//  Login_SwiftUI
+//
+//  Created by home on 2020/10/27.
+//
+
+import SwiftUI
+
+struct LineLoginButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LineLoginButton_Previews: PreviewProvider {
+    static var previews: some View {
+        LineLoginButton()
+    }
+}
