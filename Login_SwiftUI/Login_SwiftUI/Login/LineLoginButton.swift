8c6060257d9b802d3d13e29bf9a0723cdd77dc67
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-10-29 15:09:04 +0900

    コメントを追加

--- Login_SwiftUI/Login_SwiftUI/Login/LineLoginButton.swift
+++ Login_SwiftUI/Login_SwiftUI/Login/LineLoginButton.swift
@@ -31,23 +31,34 @@ struct LineLoginButton: UIViewRepresentable {
         }
     }
     
+    // UIViewRepresentableに準拠したView（構造体LineLoginButton）はCoordinator型を定義できる
+    // Coordinator型はSwiftUIの仕組みで、
+    // UIViewRepresentableに準拠したViewの状況（Context）の一部を他のSwiftUIのインターフェースに伝達できるようにするもの
+    // makeCoordinatorで生成したCoordinatorのインスタンスにはupdateUIViewの引数contextからアクセスする
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
     
+    // Viewを初期化
     func makeUIView(context: Context) -> UIView {
         let view = UIView()
         return view
     }
     
+    // Viewの状態を更新
     func updateUIView(_ view: UIView, context: Context) {
         let loginButton = LoginButton()
+        // makeCoordinatorで生成したCoordinatorクラスのインスタンスを指定
         loginButton.delegate = context.coordinator
         
+        // 認証時にユーザーへプロフィール情報の取得について許可をとる
         loginButton.permissions = [.profile]
+        // nilを指定すると、現在のViewController階層の最上位のViewControllerが使用される
         loginButton.presentingViewController = nil
         
+        // makeUIViewで生成したViewにLINE SDKで用意されたLoginButtonを追加
         view.addSubview(loginButton)
+        // ログインボタンのレイアウト設定
         loginButton.translatesAutoresizingMaskIntoConstraints = false
         loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         loginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
